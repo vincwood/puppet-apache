4b9ab7047aca033c66ce36f6e11eed498c57dd4b
root <root@vinsunfr1c.mylabserver.com>
2019-06-21 08:04:28 +0000

    Ajout Class Params

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,11 @@
 # @Summary
 #   Install the base Apache package
 #
-class apache::install {
-  package { 'httpd':
-    ensure => present,
+class apache::install (
+  $install_name   = $apache::params::install_name,
+  $install_ensure = $apache::params::install_ensure,
+) inherits apache::params {
+  package { "$install_name":
+    ensure => $install_ensure,
   }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# @summar
+#   Operating System-related variables (and others)
+#
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
